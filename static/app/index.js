fad1d9d4137113e8d1cd415620db6d49949cf274
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-06 19:45:14 -0500

    Add webpack, broken Sass compilation

--- static/app/index.js
+++ static/app/index.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
-import FooTemplate from '../assets/templates/foo.tpl!';
-import '../assets/styles/style.sass!';
+import 'assets/styles/style.sass';
 
 console.log(BENCHSPEC);
 console.log(FooTemplate({example: 'foobar'}));
