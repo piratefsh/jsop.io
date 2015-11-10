4bfa2d15c654bab903e166c414f56fb7f2fd1e7d
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-09 20:10:32 -0500

    Add SystemJS CDN

--- static/app/index.js
+++ static/app/index.js
@@ -3,4 +3,13 @@ import 'assets/styles/style.sass';
 
 console.log(BENCHSPEC);
 console.log(_.VERSION);
+console.log(System);
 
+System.transpiler = 'traceur';
+const p = System.import('http://code.jquery.com/jquery-1.11.3.min.js')
+p.then(function(m){
+    console.log(m)
+})
+// require.ensure(['script!//code.jquery.com/jquery-1.11.3.min.js'], (require) => {
+//     console.log($)
+// });
