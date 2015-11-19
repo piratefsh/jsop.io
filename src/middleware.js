0f75bc23cbd592f48a5c3315db9fe4d5901bcb51
Harry Truong <harry@asdfjkltech.com>
2015-11-18 20:51:36 -0500

    CORS method fix

--- src/middleware.js
+++ src/middleware.js
@@ -38,6 +38,7 @@ module.exports = app => [
   // enable CORS
   function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept');
